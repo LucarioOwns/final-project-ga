ab9073fad875543daca447a0050b68f5ba4f80da
Roy Frias <roy.frias2@gmail.com>
2019-10-08 20:49:34 -0400

    Other Small Changes

--- js/project.js
+++ js/project.js
@@ -1,39 +1,11 @@
-// $("button").on("click", function(){
-//   $(".book1").fadeIn(2000, function(){
-//     console.log("It\'s Working!!!!");
-//   });
-// });
-
-
-
-  $("button").on("click", function () {
-    
-    // $(".book").fadeIn(2000);
-  
-
-    for (let i = 0; i < book.length; i++) {
-
-
-      $('#books').append(
-        '<div class="book" >' +
-        '<img src="' + book[i].image + '" alt="cover-page" class="image">' +
-        '<div class="side-info">' + 
-        '<p>Title : ' + book[i].title + '</p>' +
-        '<p>Publisher : ' + book[i].publisher + '</p>' +
-        '<p>Price : ' + book[i].price + '</p>' +
-        '</div>' +
-        '</div>'
-       
-      );
-     
-    }
-    $(".book").fadeIn(2000);
-
-  });
-
-  
 let book = [
 
+  {
+    title: 'Space The Final Frontier',
+    publisher: 'Ownage Books',
+    price: '$9.97',
+    image: 'images/FinalFrontier.png'
+  },
   {
     title: 'Opposite Attractions',
     publisher: 'Independent House',
@@ -95,8 +67,130 @@ let book = [
     price: '$26.95',
     image: 'images/Steak.jpg'
   },
- 
+
+  
 ];
+
+const bookList = $('input');
+let bookTitleLowercase;
+let inputValLowercase;
+
+$('#search-bar').on('submit', function(event) {
+  event.preventDefault();
+  const inputVal = $('#search-bar input').val();
+  console.log(inputVal);
+  // const booksArr = book[i].title;
+  // console.log(booksArr);
+  $(".book").remove();
+
+  for (let i = 0; i < book.length; i++) {
+    bookTitleLowercase = book[i].title.toLowerCase();
+    inputValLowercase = inputVal.toLowerCase();
+    if (bookTitleLowercase.includes(inputValLowercase) === true) {
+      $('#books').append(
+        '<div class="book" >' +
+        '<img src="' + book[i].image + '" alt="cover-page" class="image">' +
+        '<div class="side-info">' +
+        '<p>Title : ' + book[i].title + '</p>' +
+        '<p>Publisher : ' + book[i].publisher + '</p>' +
+        '<p>Price : ' + book[i].price + '</p>' +
+        '</div>' +
+        '</div>'
+      );
+    } else if (bookTitleLowercase.includes(inputValLowercase) === false)  {
+      $('#search-bar input').html(console.log('No results')).val();
+      // console.log('No results');
+      
+    }
+  }
+  $(".book").fadeIn(2000);
+  
+});
+
+/* WORKING CODE
+
+  $("button").on("click", function () {
+
+    // $(".book").fadeIn(2000);
+
+    for (let i = 0; i < book.length; i++) {
+
+
+      $('#books').append(
+        '<div class="book" >' +
+        '<img src="' + book[i].image + '" alt="cover-page" class="image">' +
+        '<div class="side-info">' +
+        '<p>Title : ' + book[i].title + '</p>' +
+        '<p>Publisher : ' + book[i].publisher + '</p>' +
+        '<p>Price : ' + book[i].price + '</p>' +
+        '</div>' +
+        '</div>'
+
+      );
+
+    }
+    $(".book").fadeIn(2000);
+
+  });
+
+*/
+
+
+
+
+
+
+// OTHER CODE
+
+// $("button").on("click", function(){
+//   $(".book1").fadeIn(2000, function(){
+//     console.log("It\'s Working!!!!");
+//   });
+// });
+
+// $("input").on('keypress', function (e) {
+//   console.log(e.target.value);
+// });
+
+// $('input').on('keypress', function (e) {
+//   const filter = book.filter(function (book) {
+//     const regex = new RegExp(e.target.value, 'gi');
+//     return book.title.match(regex);
+//   });
+
+//   console.log(filter);
+// });
+
+// $("input").on("keypress", function (event) {
+//   const c = book.filter(function (b) {
+//     return b.title.match(new RegExp(event.target.value, "gi"));
+//   });
+//   console.log(c);
+// });
+
+// const  bookList = $('#search');
+
+// $('#search').on('keypress', function(){
+//   const bookFilter = book.filter( function(book){
+//     $('#search').val('');
+//     console.log('#search');
+//   });
+// });
+
+
+// $("#search").on('keypress', function (e) {
+//   console.log(e.target.value);
+// });
+
+// $('input').on('keypress', function (e) {
+//   const filter = books.filter(function (book) {
+//     const regex = new RegExp(e.target.value, 'gi');
+//     return book.title.match(regex);
+//   });
+
+//   console.log(filter);
+// });
+
 
 
 
